fc08e238785e8325146fc61eb61ef2d920dc793f
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-28 18:12:37 +0300

    commit tree bb249937160ed613bfdfb34ab72d561af18a3be6

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,16 +25,6 @@ class Dog
         DB[:conn].execute(sql)
     end
 
-    def save
-        sql = <<-SQL
-            INSERT INTO dogs (name, breed)
-            VALUES (?, ?)
-        SQL
-        DB[:conn].execute(sql, self.name, self.breed)
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-        self
-    end
-
     def self.create(name:, breed:)
         dog = Dog.new(name: name, breed: breed)
         dog.save
@@ -72,4 +62,58 @@ class Dog
         DB[:conn].execute(sql, id).map{|row|self.new_from_db(row)}.first
     end
 
+    # BONUS METHODS
+
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ? AND breed = ?
+            LIMIT 1
+        SQL
+        dog = DB[:conn].execute(sql, name, breed)
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
+    def update
+        sql = <<-SQL
+            UPDATE dogs
+            SET name = ?, breed = ?
+            WHERE id = ?
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    # change name or breed of a dog
+    def update_name(name)
+        self.name = name
+        self.update
+    end
+
+    def update_breed(breed)
+        self.breed = breed
+        self.update
+    end
+
+    def save
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+        end
+    end
+
 end
+
